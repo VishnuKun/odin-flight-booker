c1051b11b8a12ebce953b05cef99a90e939632ee
Vishnu <vishnukun954AV@protonmail.com>
2023-12-15 19:39:29 +0545

    Styled up the sign in and sign up page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def label_class
-    "block text-sm font-medium leading-6 text-gray-900"
+    "block text-sm font-bold leading-6 text-gray-900"
   end
 
   def input_class
-    "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+    "block w-full rounded-md border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mb-6"
   end
 
   def form_button_class
