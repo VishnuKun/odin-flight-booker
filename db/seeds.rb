fb54b4dc83744ffab6af238d634540accf85e8d3
Vishnu <vishnukun954AV@protonmail.com>
2023-12-17 10:31:22 +0545

    Configured associations and seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create airports
+airport1 = Airport.create(code: 'LAX')
+airport2 = Airport.create(code: 'SFO')
+
+# Create flights
+flight1 = Flight.create(
+  departure_airport_id: airport1.id,
+  arrival_airport_id: airport2.id,
+  start_datetime: DateTime.new(2023, 12, 16, 9, 0, 0),
+  flight_duration: 2
+)
+
+flight2 = Flight.create(
+  departure_airport_id: airport2.id,
+  arrival_airport_id: airport1.id,
+  start_datetime: DateTime.new(2023, 12, 16, 15, 0, 0),
+  flight_duration: 2
+)
