024e8042b47a8843199538cbdccc83d885f71c18
Vishnu <vishnukun954AV@protonmail.com>
2023-12-23 14:35:40 +0545

    Added associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+  has_many :bookings
+  has_many :passengers, through: :bookings
 
   def self.ransackable_attributes(auth_object = nil)
     ["arrival_airport_id", "departure_airport_id", "flight_duration", "start_datetime", "passengers"]
